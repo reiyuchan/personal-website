ca4ce0f499e7b675b779aa58e255709e4a5fd478
reiyuchan <therealsyedofficial@gmail.com>
2022-05-19 14:33:13 +0200

    changed default theme from white to dark mode

--- utils/themeswitcher.ts
+++ utils/themeswitcher.ts
@@ -1,7 +1,16 @@
+let defaultTheme = true;
+
 const switcher: any = () => {
   const root = document.querySelector('html') as HTMLHtmlElement;
   const button = document.querySelector('#switcher') as HTMLButtonElement;
+  
+  if (defaultTheme) {
+    localStorage.setItem('theme', 'dark');
+    defaultTheme = !defaultTheme;
+  }
+  
   const theme = localStorage.getItem('theme');
+
   if (theme === 'dark') {
     root.style.colorScheme = 'dark';
     button.style.colorScheme = 'dark';
