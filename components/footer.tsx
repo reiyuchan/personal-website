0fc50477d0e475d6efe53f14f44cc6ba19609d9e
reiyuchan <therealsyedofficial@gmail.com>
2022-05-19 17:33:13 +0200

    removed unnecessary code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,9 @@
-import Image from 'next/image';
-import { useEffect } from 'react';
-
 const Footer = () => {
-    useEffect(() => {
-    })
     return (
         <div>
-            <footer style={{ position: 'fixed', bottom: '0', width: '100%', backgroundColor: 'brown', padding: '5px', userSelect: 'none' }}>
-                <a style={{ display: 'block', width: '2.5rem', float: 'right', marginRight: '1.5rem' }} href="https://github.com/reiyuchan" target={'_blank'} rel="noreferrer">
-                    <img style={{width: '2.5rem'}} id='logo' src='/GitHub-Mark-Light-64px.png' alt='github page' />
+            <footer style={{ width: '100%', padding: '5px', userSelect: 'none' }}>
+                <a style={{ display: 'block', width: '2.5rem', margin: 'auto', marginTop: '2.5rem' }} href="https://github.com/reiyuchan" target={'_blank'} rel="noreferrer">
+                    <img style={{ width: '2.5rem' }} id='logo' src='/GitHub-Mark-Light-64px.png' alt='github page' />
                 </a>
             </footer>
         </div>
