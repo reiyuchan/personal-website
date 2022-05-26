147a423e04b8f30fdc126ef4b31070f7d05f3ef8
reiyuchan <therealsyedofficial@gmail.com>
2022-05-26 03:35:43 +0200

    moved props types to types folder

---
+++ types/data.d.ts
@@ -0,0 +1,13 @@
+interface BodyProps {
+  head1?: string;
+  head2?: string;
+  p1?: string;
+  p2?: string;
+  p3?: string;
+  p4?: string;
+  p5?: string;
+  p6?: string;
+  p7?: string;
+}
+
+export type { BodyProps };
